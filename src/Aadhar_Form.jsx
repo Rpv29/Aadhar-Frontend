65f5754b5c5289d188b49ddedcdc1c14c0cb465f
Preetham Varma <preethamv58@gmail.com>
2026-02-07 22:48:05 +0530

    Initial React frontend

---
+++ src/Aadhar_Form.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useState } from "react";
+
+function AadhaarForm(){
+
+    const[formdata,setformdata]=useState({
+        aadharNumber:"",
+        name:"",
+        dateofbirth:"",
+        mobile:"",
+        address:""
+
+    })
+
+    const handleChange=(e)=>{
+        setformdata({...formdata,[e.target.name]: e.target.value})
+    }
+
+    const submitdata=(e)=>{
+          e.preventDefault();
+
+          if (!/^\d{12}$/.test(formdata.aadharNumber)) {
+                alert("Aadhaar must be exactly 12 digits");
+                return;
+          }
+
+            if (!/^\d{10}$/.test(formdata.mobile)) {
+                alert("Mobile must be exactly 10 digits");
+                return;
+            }
+
+            if (!/^\d{4}-\d{2}-\d{2}$/.test(formdata.dateofbirth)) {
+                alert("Date must be in yyyy-MM-dd format");
+                return;
+            }
+
+        axios.post("http://localhost:9090/aadhar",{
+            ...formdata,
+            activeStatus: true
+        })
+        .then(()=>alert("Aadhaar details are saved Successfully"))
+        .catch(err=>{ console.log(err) ;alert("Error in creating aadhar")})
+    }
+
+    return <div>
+
+        <form action="" onSubmit={submitdata}> 
+
+            <input name="aadharNumber" type="text" placeholder="Aadhaar number" onInput={handleChange}/><br /><br />
+            <input name="name" type="text" placeholder="Name" onInput={handleChange} /><br /><br />
+            <input name="dateofbirth" type="date" placeholder="YYYY-MM-DD" onInput={handleChange}/><br /><br />
+            <input name="mobile" type="text" placeholder="Mobile" onInput={handleChange}/><br /><br />
+            <input name="address" type="text" placeholder="Address" onInput={handleChange} /><br /><br />
+
+            <button>Submit</button>
+        </form>
+    </div>
+    
+}
+
+export default AadhaarForm
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AadhaarForm from "./Aadhar_Form"
+
+function App() {
+  return (
+    <div>
+
+        <h1>AAdhaar Management System</h1>
+
+        <AadhaarForm></AadhaarForm>
+      
+    </div>
+  )
+}
+
+export default App
